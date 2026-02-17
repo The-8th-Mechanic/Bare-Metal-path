2b5cc5bd14ffeeb4b1b6f7f5740ecb9c51427696
The-8th-Mechanic <ebenezeradgbola2004@gmail.com>
2026-02-17 12:44:55 +0100

    done with LED PWM

--- 04_control_LED_with_PWM/src/main.cpp
+++ 04_control_LED_with_PWM/src/main.cpp
@@ -1,25 +1,26 @@
 #include <Arduino.h>
 
 void setup () {
-DDRD |= (1<<PD2);
+DDRD |= (1<<PD2); //Turn on the LED connected to D2(Pin 2).
 
 }
 
 void loop () {
-for (int brightness=0; brightness<=1000; brightness +=10) {
-  PORTD |=(1<<PD2);
-  delayMicroseconds(brightness);
+for (int i=0;i<=1000;i+=2) {
+  PORTD |= (1<<PD2);
+  delayMicroseconds(i);
   PORTD &= ~(1<<PD2);
-  delayMicroseconds(1000-brightness);
+  delayMicroseconds(1000-i);
 }
- for (int brightness=1000;brightness>=0; brightness -= 10) {
-PORTD |= (1<<PD2);
-delayMicroseconds(brightness);
-PORTD &= ~(1<<PD2);
-delayMicroseconds(1000-brightness);
 
-
+for (int i=1000;i>=0;i-=2) {
+  PORTD |= (1<<PD2);
+  delayMicroseconds(i);
+  PORTD &= ~(1<<PD2);
+  delayMicroseconds(1000-i);
 }
+
+delay(1000);
 
 
 }
